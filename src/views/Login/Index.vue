8c9dbcd894454d3a06f5893f43e7f107bda31958
Fredic Yohany Rojas Parra <rojas.fredic6@gmail.com>
2021-11-02 17:24:12 -0500

    config

---
+++ src/views/Login/Index.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+.home
+  p Holanda
+  .button button
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "Home",
+  components: {},
+})
+export default class Home extends Vue {}
+</script>
